f3ffe97f18e798f29d704d34fe4072d5c6a0e04b
Satnam Brar <satnamsbrar@gmail.com>
2017-01-28 14:16:08 -0500

    created account base class

---
+++ BankAccount/Checking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Checking : Account
+    {
+        //fields
+        protected string lastName;
+        protected string firstName;
+
+        protected double totalBalance;
+
+        //properties
+        public string FullName
+        {
+            get { return lastName + ", " + firstName; }
+            set { lastName = value;  firstName = value; }
+        }
+        public double TotalBalance
+        {
+            get { return totalBalance; }
+            set { totalBalance = value; }
+        }
+    }
+}
